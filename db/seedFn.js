e271387ffa001a0cb39d49a54d33c542eaaca6d3
= <=>
2023-04-13 20:07:04 +0100

    done

--- db/seedFn.js
+++ db/seedFn.js
@@ -5,25 +5,13 @@ const bcrypt = require("bcrypt");
 
 const seed = async () => {
   await sequelize.sync({ force: true }); // recreate db
-  await User.bulkCreate(users);
   await Promise.all(
     users.map(async (user) => {
-      const SALT_LENGTH = 10;
-      const hashedPassword = await bcrypt.hash(user.password, SALT_LENGTH);
+      const hashedPassword = await bcrypt.hash(user.password, 10);
       user.password = hashedPassword;
     })
   );
+  await User.bulkCreate(users);
 };
-
-// async function hashPassword() {
-//   const SALT_LENGTH = 10;
-//   const users = await User.findAll();
-//   await Promise.all(
-//     users.map(async (user) => {
-//       const hashedPassword = await bcrypt.hash(user.password, SALT_LENGTH);
-//       user.password = hashedPassword;
-//     })
-//   );
-// }
 
 module.exports = seed;
